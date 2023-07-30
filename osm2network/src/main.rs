f50c0430e204c52cfb6b27add5c81635292b0891
Dustin Carlino <dabreegster@gmail.com>
2023-07-30 16:25:58 +0100

    Read them in from osm2network and include in output
    ... revealing that OSRM includes intermediate nodes in the output, as
    suspected

--- osm2network/src/main.rs
+++ osm2network/src/main.rs
@@ -1,48 +1,76 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufWriter, Write};
+use std::io::{BufReader, BufWriter, Write};
 use std::time::Instant;
 
+use clap::Parser;
 use geojson::{Feature, Geometry, JsonObject, JsonValue, Value};
 use osmpbf::{Element, ElementReader};
 use serde::{Deserialize, Serialize};
 
+#[derive(Parser)]
+#[clap(about, version, author)]
+struct Args {
+    // TODO subcommands, really
+    #[clap(long)]
+    osm_pbf_input: Option<String>,
+
+    #[clap(long)]
+    bin_input: Option<String>,
+
+    #[clap(long)]
+    counts: Option<String>,
+}
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        panic!("Give a .osm.pbf as input");
-    }
+    let args = Args::parse();
 
-    println!("Scraping {}", args[1]);
-    let mut start = Instant::now();
-    let (nodes, ways) = scrape_elements(&args[1]);
-    println!(
-        "Got {} nodes and {} ways. That took {:?}",
-        nodes.len(),
-        ways.len(),
-        Instant::now().duration_since(start)
-    );
+    let network: Network = if let Some(path) = args.osm_pbf_input {
+        println!("Scraping {path}");
+        let mut start = Instant::now();
+        let (nodes, ways) = scrape_elements(&path);
+        println!(
+            "Got {} nodes and {} ways. That took {:?}",
+            nodes.len(),
+            ways.len(),
+            Instant::now().duration_since(start)
+        );
 
-    start = Instant::now();
-    let network = split_edges(nodes, ways);
-    println!(
-        "Split into {} edges. That took {:?}",
-        network.edges.len(),
-        start
-    );
+        start = Instant::now();
+        let network = split_edges(nodes, ways);
+        println!(
+            "Split into {} edges. That took {:?}",
+            network.edges.len(),
+            start
+        );
 
-    {
         println!("Saving to network.bin");
         start = Instant::now();
         let writer = BufWriter::new(File::create("network.bin").unwrap());
         bincode::serialize_into(writer, &network).unwrap();
         println!("That took {:?}", Instant::now().duration_since(start));
-    }
+
+        network
+    } else if let Some(path) = args.bin_input {
+        bincode::deserialize_from(BufReader::new(File::open(path).unwrap())).unwrap()
+    } else {
+        panic!("Give input");
+    };
 
     println!("Saving to network.geojson");
-    start = Instant::now();
-    network.write_geojson("network.geojson");
+    let start = Instant::now();
+    if let Some(path) = args.counts {
+        let counts = bincode::deserialize_from(BufReader::new(File::open(path).unwrap())).unwrap();
+        network.write_geojson_with_counts("network.geojson", counts);
+    } else {
+        network.write_all_geojson("network.geojson");
+    }
     println!("That took {:?}", Instant::now().duration_since(start));
+}
+
+#[derive(Deserialize)]
+struct Counts {
+    count_per_edge: HashMap<(i64, i64), usize>,
 }
 
 #[derive(Clone, Copy, Serialize, Deserialize)]
@@ -68,6 +96,31 @@ struct Edge {
     way_id: i64,
     tags: Vec<(String, String)>,
     geometry: Vec<Position>,
+}
+
+impl Edge {
+    fn to_geojson(&self, node1: i64, node2: i64) -> Feature {
+        let geometry = Geometry::new(Value::LineString(
+            self.geometry
+                .iter()
+                .map(|pt| vec![1e-7 * pt.lon as f64, 1e-7 * pt.lat as f64])
+                .collect(),
+        ));
+        let mut properties = JsonObject::new();
+        for (key, value) in &self.tags {
+            properties.insert(key.to_string(), JsonValue::from(value.to_string()));
+        }
+        properties.insert("node1".to_string(), JsonValue::from(node1));
+        properties.insert("node2".to_string(), JsonValue::from(node2));
+        properties.insert("way".to_string(), JsonValue::from(self.way_id));
+        Feature {
+            bbox: None,
+            geometry: Some(geometry),
+            id: None,
+            properties: Some(properties),
+            foreign_members: None,
+        }
+    }
 }
 
 fn scrape_elements(path: &str) -> (HashMap<i64, Position>, HashMap<i64, Way>) {
@@ -166,7 +219,7 @@ fn split_edges(nodes: HashMap<i64, Position>, ways: HashMap<i64, Way>) -> Networ
 }
 
 impl Network {
-    fn write_geojson(&self, path: &str) {
+    fn write_all_geojson(&self, path: &str) {
         // Write one feature at a time manually, to avoid memory problems
         let mut file = BufWriter::new(File::create(path).unwrap());
         writeln!(file, "{{\"type\":\"FeatureCollection\", \"features\":[").unwrap();
@@ -177,28 +230,31 @@ impl Network {
             } else {
                 add_comma = true;
             }
-
-            let geometry = Geometry::new(Value::LineString(
-                edge.geometry
-                    .iter()
-                    .map(|pt| vec![1e-7 * pt.lon as f64, 1e-7 * pt.lat as f64])
-                    .collect(),
-            ));
-            let mut properties = JsonObject::new();
-            for (key, value) in &edge.tags {
-                properties.insert(key.to_string(), JsonValue::from(value.to_string()));
-            }
-            properties.insert("node1".to_string(), JsonValue::from(*node1));
-            properties.insert("node2".to_string(), JsonValue::from(*node2));
-            properties.insert("way".to_string(), JsonValue::from(edge.way_id));
-            let feature = Feature {
-                bbox: None,
-                geometry: Some(geometry),
-                id: None,
-                properties: Some(properties),
-                foreign_members: None,
-            };
+            let feature = edge.to_geojson(*node1, *node2);
             serde_json::to_writer(&mut file, &feature).unwrap();
+        }
+        writeln!(file, "]}}").unwrap();
+    }
+
+    fn write_geojson_with_counts(&self, path: &str, counts: Counts) {
+        // Write one feature at a time manually, to avoid memory problems
+        let mut file = BufWriter::new(File::create(path).unwrap());
+        writeln!(file, "{{\"type\":\"FeatureCollection\", \"features\":[").unwrap();
+        let mut add_comma = false;
+
+        for ((node1, node2), count) in counts.count_per_edge {
+            if let Some(edge) = self.edges.get(&(node1, node2)) {
+                if add_comma {
+                    writeln!(file, ",").unwrap();
+                } else {
+                    add_comma = true;
+                }
+                let mut feature = edge.to_geojson(node1, node2);
+                feature.set_property("count", count);
+                serde_json::to_writer(&mut file, &feature).unwrap();
+            } else {
+                println!("No edge from https://www.openstreetmap.org/node/{node1} to https://www.openstreetmap.org/node/{node2}");
+            }
         }
         writeln!(file, "]}}").unwrap();
     }
