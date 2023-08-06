71aafad9591e6f508ee00d53b65f9864c025173b
Dustin Carlino <dabreegster@gmail.com>
2023-08-06 12:21:47 +0100

    Organize OSRM code together

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod input;
 mod node_map;
 mod od;
 mod osm2network;
+mod osrm;
 mod requests;
 mod tags;
 
@@ -10,8 +11,7 @@ use std::time::Instant;
 
 use anyhow::Result;
 use clap::Parser;
-use futures::{stream, StreamExt};
-use indicatif::{HumanCount, ProgressBar, ProgressStyle};
+use indicatif::HumanCount;
 
 #[derive(Parser)]
 #[clap(about, version, author)]
@@ -69,7 +69,7 @@ async fn main() -> Result<()> {
     start = Instant::now();
     let counts = match config.routing {
         input::Routing::OSRM { concurrency } => {
-            run_with_osrm(&network, requests, concurrency.unwrap_or(10)).await?
+            osrm::run(&network, requests, concurrency.unwrap_or(10)).await?
         }
         input::Routing::Custom => custom_routing::run(&network, requests)?,
     };
@@ -87,67 +87,4 @@ async fn main() -> Result<()> {
     println!("That took {:?}", Instant::now().duration_since(start));
 
     Ok(())
-}
-
-async fn run_with_osrm(
-    network: &osm2network::Network,
-    requests: Vec<requests::Request>,
-    concurrency: usize,
-) -> Result<osm2network::Counts> {
-    let num_requests = requests.len();
-    println!(
-        "Making {} requests with concurrency = {}",
-        HumanCount(num_requests as u64),
-        concurrency
-    );
-
-    let results = stream::iter(requests)
-        .map(|req| tokio::spawn(async { req.calculate_route().await }))
-        .buffer_unordered(concurrency);
-
-    // Count routes per node pairs
-    let progress = ProgressBar::new(num_requests as u64).with_style(ProgressStyle::with_template(
-            "[{elapsed_precise}] [{wide_bar:.cyan/blue}] {human_pos}/{human_len} ({per_sec}, {eta})").unwrap());
-    let mut counts = osm2network::Counts::new();
-    results
-        .fold(&mut counts, |accumulate, future| async {
-            progress.inc(1);
-            // TODO Flatten
-            match future {
-                Ok(result) => match result {
-                    Ok(nodes) => {
-                        // OSRM returns all nodes, but we only consider some to be intersections
-                        // TODO When the route begins or ends with an intermediate non-intersection
-                        // node, we don't handle it well yet
-                        let mut i1 = nodes[0];
-                        let mut last = nodes[0];
-                        for node in nodes.into_iter().skip(1) {
-                            if network.intersections.contains_key(&node) {
-                                *accumulate.count_per_edge.entry((i1, node)).or_insert(0) += 1;
-                                i1 = node;
-                            }
-                            last = node;
-                        }
-                        if i1 != last && false {
-                            println!("We didn't end on an intersection... {i1} to {last}");
-                        }
-                    }
-                    Err(err) => {
-                        // TODO Usually the API being overloaded
-                        if false {
-                            println!("Request failed: {err}");
-                        }
-                        accumulate.errors += 1;
-                    }
-                },
-                Err(err) => {
-                    println!("Tokio error: {err}");
-                }
-            }
-            accumulate
-        })
-        .await;
-    progress.finish();
-
-    Ok(counts)
 }
--- src/requests.rs
+++ src/requests.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use geojson::{GeoJson, Value};
-use reqwest::Client;
 
 pub struct Request {
     pub x1: f64,
@@ -10,34 +9,6 @@ pub struct Request {
 }
 
 impl Request {
-    // Returns OSM node IDs, using OSRM
-    pub async fn calculate_route(self) -> Result<Vec<i64>> {
-        // TODO How to share, and does it matter?
-        let client = Client::new();
-
-        // Alternatively, try bindings (https://crates.io/crates/rsc_osrm)
-        let body = client
-            .get(format!(
-                "http://localhost:5000/route/v1/driving/{},{};{},{}",
-                self.x1, self.y1, self.x2, self.y2
-            ))
-            .query(&[
-                ("overview", "false"),
-                ("alternatives", "false"),
-                ("steps", "false"),
-                ("annotations", "nodes"),
-            ])
-            .send()
-            .await?
-            .text()
-            .await?;
-        let json_value: serde_json::Value = serde_json::from_str(&body)?;
-        let nodes: Vec<i64> = serde_json::from_value(
-            json_value["routes"][0]["legs"][0]["annotation"]["nodes"].clone(),
-        )?;
-        Ok(nodes)
-    }
-
     pub fn load_from_geojson(
         path: &str,
         sample_requests: usize,
