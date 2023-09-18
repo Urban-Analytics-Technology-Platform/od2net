28ec65aaa1ec66b7a6f37d0732080ea7a95fcade
Dustin Carlino <dabreegster@gmail.com>
2023-09-18 11:09:47 +0100

    Don't break popup if we skip OSM tags

--- viewer/src/PropertiesTable.svelte
+++ viewer/src/PropertiesTable.svelte
@@ -3,28 +3,38 @@
 
   export let properties: { [name: string]: any };
 
-  // Get detailed messages. Remember maplibre compacts nested feature properties, so we have to parse as JSON!
-  let tags = JSON.parse(properties.osm_tags);
-  let { messages } = calculate({
-    method: "bike_ottawa",
-    tags,
-  });
+  // Get detailed LTS messages, if we preserved OSM tags
+  let ltsMessages = ["No LTS info, because OSM tags were dropped"];
+  let tags = {};
+  if (properties.osm_tags) {
+    // Remember maplibre compacts nested feature properties, so we have to parse as JSON!
+    tags = JSON.parse(properties.osm_tags);
+    let { messages } = calculate({
+      method: "bike_ottawa",
+      tags,
+    });
+    ltsMessages = messages;
+  }
 </script>
 
-<b>Count: {properties.count.toFixed(2)}</b>
-<b>LTS: {properties.lts}</b>
+<div><b>Count: {properties.count.toFixed(2)}</b></div>
+<div><b>LTS: {properties.lts}</b></div>
 <ul>
-  {#each messages as msg}
+  {#each ltsMessages as msg}
     <li>{msg}</li>
   {/each}
 </ul>
-<table>
-  <tbody>
-    {#each Object.entries(tags) as [key, value]}
-      <tr><td>{key}</td><td>{value}</td></tr>
-    {/each}
-  </tbody>
-</table>
+{#if properties.osm_tags}
+  <table>
+    <tbody>
+      {#each Object.entries(tags) as [key, value]}
+        <tr><td>{key}</td><td>{value}</td></tr>
+      {/each}
+    </tbody>
+  </table>
+{:else}
+  <p>OSM tags weren't kept</p>
+{/if}
 
 <style>
   table {
