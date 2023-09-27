3dd0ce15f69e1d4f2c4b324041ccc3ecb871b969
Dustin Carlino <dabreegster@gmail.com>
2023-09-27 12:49:44 +0100

    Add uptake to individual route output. #26

--- aggregate_routes/src/detailed_route_output.rs
+++ aggregate_routes/src/detailed_route_output.rs
@@ -2,10 +2,12 @@ use std::io::BufWriter;
 
 use anyhow::Result;
 use fs_err::File;
+use geojson::JsonObject;
 
 use super::config::{CostFunction, Uptake};
 use super::custom_routing::{build_ch, build_closest_intersection, PreparedCH};
 use super::osm2network::Network;
+use super::plugins;
 use super::requests::Request;
 use super::timer::Timer;
 
@@ -45,6 +47,7 @@ pub fn run(
                 path,
                 &prepared_ch,
                 network,
+                uptake,
             )?;
             if i == num_requests {
                 break;
@@ -61,11 +64,15 @@ fn output_detailed_route(
     path: fast_paths::ShortestPath,
     prepared_ch: &PreparedCH,
     network: &Network,
+    uptake: &Uptake,
 ) -> Result<()> {
     // TODO Include uptake and stats about the entire route
 
     let mut features = Vec::new();
 
+    // fast_paths returns the total cost, but it's not necessarily the right unit.
+    // Calculate how long this route is.
+    let mut total_distance = 0.0;
     for pair in path.get_nodes().windows(2) {
         let i1 = prepared_ch.node_map.translate_id(pair[0]);
         let i2 = prepared_ch.node_map.translate_id(pair[1]);
@@ -75,12 +82,18 @@ fn output_detailed_route(
             (network.edges.get(&(i2, i1)).unwrap(), false)
         };
         features.push(edge.to_geojson_for_detailed_output(i1, i2, geometry_forwards));
+        total_distance += edge.length_meters;
     }
+
+    let count = plugins::uptake::calculate_uptake(uptake, total_distance);
+    let mut foreign_members = JsonObject::new();
+    foreign_members.insert("uptake".to_string(), count.into());
+    foreign_members.insert("total_distance_meters".to_string(), total_distance.into());
 
     let gj = geojson::FeatureCollection {
         features,
         bbox: None,
-        foreign_members: None,
+        foreign_members: Some(foreign_members),
     };
     let mut file = BufWriter::new(File::create(filename)?);
     serde_json::to_writer(&mut file, &gj)?;
