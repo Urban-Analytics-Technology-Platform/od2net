bf7a2bb8a1de3f0f81a1cdd7284708b998a92983
Dustin Carlino <dabreegster@gmail.com>
2023-10-27 14:26:52 +0100

    Take file from CLI

--- tippe/src/main.rs
+++ tippe/src/main.rs
@@ -8,39 +8,22 @@ use mvt::{GeomEncoder, GeomType, MapGrid, Tile, TileId};
 use pmtiles2::{util::tile_id, Compression, PMTiles, TileType};
 use pointy::Transform;
 
-static A: f64 = 6378137.0;
-static MAXEXTENT: f64 = 20037508.342789244;
-static D2R: f64 = f64::consts::PI / 180.0;
-static R2D: f64 = 180.0 / f64::consts::PI;
-
-pub fn forward(c: [f64; 2]) -> [f64; 2] {
-    [
-        (A * c[0] * D2R).max(-MAXEXTENT).min(MAXEXTENT) as f64,
-        (A * (((f64::consts::PI * 0.25f64) + (0.5f64 * c[1] * D2R)).tan()).ln())
-            .max(-MAXEXTENT)
-            .min(MAXEXTENT) as f64,
-    ]
-}
-
-pub fn inverse(c: [f64; 2]) -> [f64; 2] {
-    [
-        (c[0] * R2D / A) as f64,
-        ((f64::consts::PI * 0.5) - 2.0 * ((-c[1] / A).exp()).atan()) * R2D,
-    ]
-}
-
 fn main() -> Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        panic!("Pass in a .geojson file");
+    }
+
     let web_mercator_transform = MapGrid::default();
 
     // Everything
     let current_tile_id = TileId::new(0, 0, 0)?;
     let transform = web_mercator_transform.tile_transform(current_tile_id);
-    // TODO whats the extent supposed to be?
     let mut tile = Tile::new(4096);
 
     let mut layer = tile.create_layer("layer1");
 
-    let reader = FeatureReader::from_reader(BufReader::new(File::open("./example.geojson")?));
+    let reader = FeatureReader::from_reader(BufReader::new(File::open(&args[1])?));
 
     for feature in reader.features() {
         let feature = feature?;
@@ -51,8 +34,8 @@ fn main() -> Result<()> {
                 for pt in line_string {
                     //Transform to mercator
                     let mercator_pt = forward([pt[0], pt[1]]);
-                    //Transform to 0-1 tile coords (not sure why this doesnt work with passing the
-                    //transform through)
+                    // Transform to 0-1 tile coords (not sure why this doesnt work with passing the
+                    // transform through)
                     let transformed_pt = transform * (mercator_pt[0], mercator_pt[1]);
                     b = b.point(transformed_pt.x * 4096.0, transformed_pt.y * 4096.0)?;
                 }
@@ -108,4 +91,18 @@ fn main() -> Result<()> {
     pmtiles.to_writer(&mut file)?;
 
     Ok(())
+}
+
+static A: f64 = 6378137.0;
+static MAXEXTENT: f64 = 20037508.342789244;
+static D2R: f64 = f64::consts::PI / 180.0;
+
+// WGS84 to Mercator
+fn forward(c: [f64; 2]) -> [f64; 2] {
+    [
+        (A * c[0] * D2R).max(-MAXEXTENT).min(MAXEXTENT) as f64,
+        (A * (((f64::consts::PI * 0.25f64) + (0.5f64 * c[1] * D2R)).tan()).ln())
+            .max(-MAXEXTENT)
+            .min(MAXEXTENT) as f64,
+    ]
 }
