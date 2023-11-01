eb0160468c0d34d20f8dcea68d60b89f6cbbf78b
Dustin Carlino <dabreegster@gmail.com>
2023-11-01 09:44:39 +0000

    Get the interactive page to deploy to GH pages

--- viewer/vite.config.ts
+++ viewer/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+        x2: resolve(__dirname, "interactive.html"),
       },
     },
   },
