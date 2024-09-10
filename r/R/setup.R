0a66a15ee14ff3c9559a46560a66325e3169c803
robinlovelace <rob00x@gmail.com>
2024-09-10 10:36:39 +0100

    Changes for all R pkg checks to pass

--- r/R/setup.R
+++ r/R/setup.R
@@ -42,10 +42,25 @@ make_osm = function(
   system(msg)
 }
 
-make_origins = function() {
-  buildings = sf::read_sf("input/input.osm.pbf", query = "SELECT osm_id FROM multipolygons WHERE building IS NOT NULL")
+#' make_origins function
+#'
+#' This function reads an OpenStreetMap file, selects the multipolygons with a non-null building attribute,
+#' calculates the centroids of the selected buildings, and writes the resulting centroids to a GeoJSON file.
+#' 
+#' @param osm_file The file path or name of the OSM file in PBF format. Default is "input/input.osm.pbf".
+#' @param query The SQL query to select the multipolygons with a non-null building attribute. Default is "SELECT osm_id FROM multipolygons WHERE building IS NOT NULL".
+#' @param output_file The file path or name of the output GeoJSON file containing the centroids of the selected buildings. Default is "input/buildings.geojson".
+#'
+#' @return None
+#' @export
+make_origins = function(
+  osm_file = "input/input.osm.pbf",
+  query = "SELECT osm_id FROM multipolygons WHERE building IS NOT NULL",
+  output_file = "input/buildings.geojson"
+  ) {
+  buildings = sf::read_sf(osm_file, query = query)
   use_sf = sf::sf_use_s2(FALSE)
   centroids = sf::st_centroid(buildings)
   sf::sf_use_s2(use_sf)
-  sf::write_sf(centroids, "input/buildings.geojson", delete_dsn = TRUE)
+  sf::write_sf(centroids, output_file, delete_dsn = TRUE)
 }
